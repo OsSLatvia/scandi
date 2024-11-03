1fca28669505c718aa737e0fed8deb5397a79659
OsSLatvia <RTL@inbox.lv>
2024-11-03 20:29:04 +0200

    added workflow to automaticly build and deploy to server

--- src/config.js
+++ src/config.js
@@ -1,10 +1,9 @@
 // src/config.js
 const config = {
-    serverLocation: 'http://localhost:80',
-    APIAddLocation: '/API/controller/ProductController.php?action=addProduct',
-    APIDeleteLocation: '/API/controller/ProductController.php?action=deleteProduct',
-    APIGetLocation: '/API/controller/ProductController.php?action=getProducts'
-  };
-  
-  export default config;
-  
+  serverLocation: 'http://localhost:80',
+  APIAddLocation: '/API/controller/ProductController.php?action=addProduct',
+  APIDeleteLocation: '/API/controller/ProductController.php?action=deleteProduct',
+  APIGetLocation: '/API/controller/ProductController.php?action=getProducts'
+};
+
+export default config;
