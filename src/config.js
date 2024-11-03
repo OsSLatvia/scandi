09b74ed04eb4888a40767491bec91cc57dfb271e
OsSLatvia <RTL@inbox.lv>
2024-11-03 22:23:51 +0200

    updated server config file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-  serverLocation: 'http://localhost:80',
+  serverLocation: 'http://scandiwebtestassignmentoskarsstepanovs.42web.io',
   APIAddLocation: '/API/controller/ProductController.php?action=addProduct',
   APIDeleteLocation: '/API/controller/ProductController.php?action=deleteProduct',
   APIGetLocation: '/API/controller/ProductController.php?action=getProducts'
